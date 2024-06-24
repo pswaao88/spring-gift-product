6dc23a5bb98ec41256edc78aaeac7c403ee63cfb
pswaao88 <pswaao88@gmail.com>
2024-06-25 00:01:03 +0900

    Refactor: Product클래스 작성

--- src/main/java/gift/Product.java
+++ src/main/java/gift/Product.java
@@ -1,5 +1,15 @@
 package java.gift;
 
 public class Product {
+    long id;
+    String name;
+    int price;
+    String imageUrl;
+    public Product(long id, String name, int price, String imageUrl){
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.imageUrl = imageUrl;
+    }
 
 }
