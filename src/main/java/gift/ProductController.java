c905cdc6b63b8da7c8f181a50d3ff8a83979c4bf
pswaao88 <pswaao88@gmail.com>
2024-06-26 08:45:22 +0900

    Refactor: 각 메소드 리턴값 ResponseEntity로 변경
    test코드 작성을 위해 리턴값 ResponseEntity로 변경

--- src/main/java/gift/ProductController.java
+++ src/main/java/gift/ProductController.java
@@ -5,6 +5,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,26 +21,26 @@ public class ProductController {
     private final Map<Long, Product> products = new HashMap<Long, Product>();
     // id를 통해 map에있는 정보 접근
     @GetMapping("/api/products")
-    public List<Product> getProduct(@RequestParam("id") Long id) {
-        return Collections.singletonList(products.get(id));
+    public ResponseEntity<Product> getProduct(@RequestParam("id") Long id) {
+        return ResponseEntity.ok().headers(new HttpHeaders()).body(products.get(id));
     }
     // 새로운 정보 저장
     @PostMapping("/api/products")
-    public List<Product> postProduct(@RequestBody Product newProduct){
+    public ResponseEntity<Product> postProduct(@RequestBody Product newProduct){
         products.put(newProduct.id(), new Product(newProduct.id(), newProduct.name(), newProduct.price(), newProduct.imageUrl()));
-        return Collections.singletonList(products.get(newProduct.id()));
+        return ResponseEntity.ok().headers(new HttpHeaders()).body(products.get(newProduct.id()));
     }
     // id에 해당되는 정보 삭제
     @DeleteMapping("/api/products")
-    public List<Product> deleteProduct(@RequestParam("id") Long id){
+    public ResponseEntity<Product> deleteProduct(@RequestParam("id") Long id){
         Product delete = products.get(id);
         products.remove(id);
-        return Collections.singletonList(delete);
+        return ResponseEntity.ok().headers(new HttpHeaders()).body(products.get(delete));
     }
     // 정보 수정
     @PutMapping("/api/products")
-    public List<Product> putProducts(@RequestBody Product newProduct){
+    public ResponseEntity<Product> putProducts(@RequestBody Product newProduct){
         products.replace(newProduct.id(), new Product(newProduct.id(), newProduct.name(), newProduct.price(), newProduct.imageUrl()));
-        return Collections.singletonList(products.get(newProduct.id()));
+        return ResponseEntity.ok().headers(new HttpHeaders()).body(products.get(newProduct.id()));
     }
 }
---
+++ src/test/java/gift/ProductControllerTest.java
@@ -0,0 +1,12 @@
+package gift;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+
+class ProductControllerTest {
+    
+}
