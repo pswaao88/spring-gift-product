7233925adec0534a0dd6dbaafe7ea978aaec6c57
pswaao88 <pswaao88@gmail.com>
2024-06-25 20:18:59 +0900

    Refactor: Get기능 메소드 작성
    ProductController클래스에 GetMapping 어노테이션을 이용해 조회 기능 작성

--- src/main/java/gift/ProductController.java
+++ src/main/java/gift/ProductController.java
@@ -1,8 +1,23 @@
 package gift;
 
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ProductController {
-    private final Map<Long, Product> products = new HashMap<>();
+
+    private final Map<Long, Product> products = new HashMap<Long, Product>();
+
+    @GetMapping("/api/products/{id}")
+    public List<Product> getProduct(@PathVariable Long id) {
+        return Collections.singletonList(products.get(id));
+    }
+
+
 }
