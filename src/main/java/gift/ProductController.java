31d1727ac60ee22377eab645e9778da5b8bc3af1
pswaao88 <pswaao88@gmail.com>
2024-06-27 21:55:08 +0900

    Feat: html파일을 작성하여 Thymleaf작성
    게시 및 추가 메소드 작성

--- src/main/java/gift/ProductController.java
+++ src/main/java/gift/ProductController.java
@@ -1,50 +1,61 @@
 package gift;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-@RestController
+@Controller
+@RequestMapping(value = "/api/products")
 public class ProductController {
 
     private final Map<Long, Product> products = new HashMap<Long, Product>();
-    // id를 통해 map에있는 정보 접근
-    @GetMapping("/api/products")
-    public ResponseEntity<Product> getProduct(@RequestParam("id") Long id) {
-        return ResponseEntity.ok().headers(new HttpHeaders()).body(products.get(id));
-    }
-    // 새로운 정보 저장
-    @PostMapping("/api/products")
-    public ResponseEntity<Product> postProduct(@RequestBody Product newProduct){
-        products.put(newProduct.id(), new Product(newProduct.id(), newProduct.name(), newProduct.price(), newProduct.imageUrl()));
-        return ResponseEntity.ok().headers(new HttpHeaders()).body(products.get(newProduct.id()));
-    }
-    // id에 해당되는 정보 삭제
-    @DeleteMapping("/api/products")
-    public ResponseEntity<Product> deleteProduct(@RequestParam("id") Long id){
-        Product delete = products.get(id);
-        products.remove(id);
-        return ResponseEntity.ok().headers(new HttpHeaders()).body(products.get(delete));
-    }
-    // 정보 수정
-    @PutMapping("/api/products")
-    public ResponseEntity<Product> putProducts(@RequestBody Product newProduct){
-        products.replace(newProduct.id(), new Product(newProduct.id(), newProduct.name(), newProduct.price(), newProduct.imageUrl()));
-        return ResponseEntity.ok().headers(new HttpHeaders()).body(products.get(newProduct.id()));
+
+    @GetMapping
+    public String getProduct(Model model) {
+        model.addAttribute("products", new ArrayList<>(products.values()));
+        return "index";
     }
 
-    public Map<Long, Product> getProducts() {
-        return products;
+    @GetMapping("/add")
+    public String addProduct(Model model) {
+        model.addAttribute("product", new Product());
+        return "post";
+    }
+
+    @PostMapping
+    public String postProduct(@ModelAttribute Product product) {
+        products.put(product.getId(), product);
+        return "redirect:/api/products";
+    }
+
+    @GetMapping("/update/{id}")
+    public String editProductF(@PathVariable Long id, Model model) {
+        Product product = products.get(id);
+        model.addAttribute("product", product);
+        return "post";
+    }
+
+    @PostMapping("/update/{id}")
+    public String updateProduct(@PathVariable Long id, @ModelAttribute Product newProduct) {
+        Product product = products.get(id);
+        product.setName(newProduct.getName());
+        product.setPrice(newProduct.getPrice());
+        product.setImageUrl(newProduct.getImageUrl());
+        products.put(id, product);
+        return "redirect:/api/products";
+    }
+
+    @PostMapping("/delete/{id}")
+    public String deleteProduct(@PathVariable Long id) {
+        products.remove(id);
+        return "redirect:/api/products";
     }
 }
