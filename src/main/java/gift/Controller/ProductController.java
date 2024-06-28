03f77c43c6356fe29edeb5ef893d3bf8b8f59fc0
pswaao88 <pswaao88@gmail.com>
2024-06-28 16:26:06 +0900

    Init: step3를 위한 패키지 및 자바 sql 파일 생성

--- src/main/java/gift/Controller/ProductController.java
+++ src/main/java/gift/Controller/ProductController.java
@@ -1,17 +1,17 @@
-package gift;
+package gift.Controller;
 
+import gift.Model.Product;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+
 @Controller
 
 public class ProductController {
--- src/main/java/gift/Model/Product.java
+++ src/main/java/gift/Model/Product.java
@@ -1,4 +1,4 @@
-package gift;
+package gift.Model;
 
 public class Product {
     private Long id;
